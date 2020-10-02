e4a16a1c12ab9ab215293cda488391283a94d991
balez4464 <71140813+balez4464@users.noreply.github.com>
2020-10-02 10:15:46 -0400

    Update Main.java
    had to manally commit

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program asks for positive interger than a negative one to quit. 
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //prompts user for a positive interger and a negative one
+    System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+    //allows user to put in interger
+    int userNumber = input.nextInt();
+
+    //while loop for users number if it is positive
+    while(userNumber >= 0){
+      System.out.println("Please enter a positive integer. Enter a negative integer to quit.");
+      //allows user to enter a new number
+      userNumber = input.nextInt();
+    }
+    //prompt for negative number
+      System.out.println("All done!");
+
+
   }
 }
